fcedfa71f9a0d8ca49b382f9caacb19ad1740cca
ruthmimo <ruthjmimo@gmail.com>
2022-04-02 13:17:10 +0300

    updated views files

--- user/views.py
+++ user/views.py
@@ -3,5 +3,5 @@ from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Instagram  app')
+    return render(request,'index.html')
 
